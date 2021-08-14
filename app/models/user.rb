233d88e9c69dc57ad6c30600739e694908485a1b
haryu <hryau6@gmail.com>
2021-08-14 11:45:17 +0900

    add: validation of user name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
-  validates :name, presence: true
+  validates :name, presence: true, length: { maximum: 10 }
   validates :email, presence: true, uniqueness: true
   validates :password_digest, presence: true
 
