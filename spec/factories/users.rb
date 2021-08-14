c259a3fffddb8a6cf986fb49a5a982a9d22fae3b
haryu <hryau6@gmail.com>
2021-08-14 14:47:02 +0900

    add: ユーザー詳細情報のエンドポイント

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :user do
     name { Faker::Name.first_name }
     email { Faker::Internet.unique.email }
+    introduction { Faker::Lorem.sentence }
     password { 'password' }
     password_confirmation { 'password' }
   end
