af35477c552fc47432ab0037673d8e0c246bd0bc
haryu <hryau6@gmail.com>
2021-08-15 16:45:41 +0900

    add: paging of microposts

--- spec/requests/api/microposts_spec.rb
+++ spec/requests/api/microposts_spec.rb
@@ -2,21 +2,37 @@ require 'rails_helper'
 
 RSpec.describe "Api::Microposts", type: :request do
   describe 'GET /api/microposts' do
-    let!(:microposts) { create_list(:micropost, 5)}
-    it '投稿の一覧取得に成功すること' do
-      get api_microposts_path
-      expect(response).to have_http_status(200)
-      json = JSON.parse(response.body)
-      expect(json['microposts']).to match_array(microposts.map{|post|
-        include(
-          'id' => post.id,
-          'content' => post.content,
-          'created_at' => be_present,
-          'updated_at' => be_present,
-          'user' => include('id' => post.user.id,
-                            'name' => post.user.name)
+    context 'ページングなし' do
+      let!(:microposts) { create_list(:micropost, 5) }
+      it 'マイクロポストの一覧が取得できること' do
+        get api_microposts_path
+        expect(response).to have_http_status(200)
+        json = JSON.parse(response.body)
+        expect(json['microposts']).to match_array(microposts.map { |micropost|
+          include(
+            'id' => micropost.id,
+            'content' => micropost.content,
+            'created_at' => be_present,
+            'updated_at' => be_present,
+            'user' => include(
+              'id' => micropost.user.id
+            )
+          )})
+      end
+    end
+
+    context 'ページングあり' do
+      let!(:microposts) { create_list(:micropost, 21) }
+      it 'マイクロポストの一覧が取得できること(pagination)' do
+        get api_microposts_path
+        expect(response).to have_http_status(200)
+        json = JSON.parse(response.body)
+        expect(json['meta']).to include(
+          'total_pages' => 3,
+          'total_count' => 21,
+          'current_page' => 1
         )
-      })
+      end
     end
   end
 
